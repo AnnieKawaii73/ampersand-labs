9c7a52024964f0bb25c3b6e7062bc015148b4b90
AnnieKawaii73 <arualanasdr@gmail.com>
2018-06-18 18:44:55 -0600

    ni

--- pages/index.js
+++ pages/index.js
@@ -6,13 +6,15 @@ class Home extends React.Component {
     render() {
         return(
             <AppLayout>
-                <h1>Make the world better.</h1>
+                <h1>Home.</h1>
                 <p>
-                    We here at Ampersand Labs believe in a better world. Our planet has a lot of potential, and it is our mission to bring the best out of it and the people that habitate here. We are in a constant search for better alternatives to bad solutions that are currently being applied to important problems, which stops humanity from reaching its true potential. 
+                Nuestra meta es mejorar la educación. Como estudiantes, nos llama la atención las muchas maneras en las que el sistema debe de mejorar, y no somos los únicos. Cada vez más personas se dan cuenta de que el sistema de educación está roto, no funciona. El problema es que aunque muchas personas lo saben, pocas actúan. La educación ha estado estancada en un mismo sitio desde hace más de 200 años y aunque es una parte fundamental de nuestra sociedad, nadie parece querer mejorarla. 
                 </p>
-                <h1>It's the little things that count.</h1>
                 <p>
-                    We aim to make small but significant changes to the world to make it a better place. Changing something little can inspire a great difference, which is our goal. By giving people the ability to make a small change in their environment, we are giving them the power to make a small section of the world better.
+                ¿Cómo podemos esperar progresar como seres humanos si no podemos educar a nuestros hijos, nuestro futuro, bien?
+                </p>
+                <p>
+                Podemos cambiar la educación de muchas maneras diferentes, pues hay muchos aspectos de ella que necesitan mejorar. Con este experimento buscamos probar que no se necesitan muchos recursos para tener un impacto positivo en los alumnos y los maestros. Los cambios que proponemos son todos relacionados con la manera en la que se ve el contenido de las clases. 
                 </p>
             </AppLayout>
         ) 
